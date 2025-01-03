f09e252dd9806be8bc129618a977a16d8b254b84
albert3289 <102903600+albert3289@users.noreply.github.com>
2025-01-03 20:13:54 +0900

    input numarr, check numarr

--- nonogram.c
+++ nonogram.c
@@ -1,1 +1,21 @@
+#include <stdio.h>
 
+int main(){
+    int arr[12][12];
+    int num[8][12];
+    for(int i = 0; i < 8; i++){
+        for(int k = 0; k < 12; k++){
+            scanf("%d", &num[i][k]);
+        }
+    }
+    for(int i = 0; i < 3; i++){
+        if(num[0][i] != 0){
+            
+        }
+    }
+    
+    
+    
+    
+    return 0;
+}
